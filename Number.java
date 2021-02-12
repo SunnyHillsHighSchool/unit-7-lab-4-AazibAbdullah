629f97140602fc0a1d497902ef8831ec6a7cc6ca
Aazib Abdullah <600021738@fjuhsd.org>
2021-02-12 07:13:51 +0000

    Wrote full Number class with private int, wrote all methods, and isOdd and isPerfect methods and toString.

--- Number.java
+++ Number.java
@@ -1,7 +1,6 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
-
+//Name - Aazib Abdullah
 
 //write the Number class
 //use the handout and
@@ -9,20 +8,61 @@
 //to guide you
 
 //header for Number class
-
+public class Number
+{
 //private instance variable (must be an Integer)
-
+  private int num;
 //constructor
-
-
+  public Number()
+  {
+    num = 0;
+  }
+  //
+  public Number(int n)
+  {
+    num = n;
+  }
 //getter method
-
-
+  public int getNum()
+  {
+    return num;
+  }
 //setter method
-
+  public void setNumber(int nn)
+  {
+    num = nn;
+  }
 //isOdd method
-
+public boolean isOdd()
+{
+  boolean b = false;
+  // If the remainder of num % 2 is 1, return it.
+  if (num % 2 == 1)
+  {
+    b = true;
+  }
+  return b;
+}
 //isPerfect method
-
-
+public boolean isPerfect()
+{
+  int sum = 0;
+  for (int i = 1; i < num; i++)
+  {
+    if (num % i == 0)
+    {
+      sum = sum + i;
+    }
+  }
+  if (sum == num)
+  {
+    return true;
+  }
+  return false;
+}
 //toString
+public String toString()
+{
+  return "The number " + num;
+}
+}
